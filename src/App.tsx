3db2611783981b6505934a2123f3b82ebd134ba3
KosmosKey <khamidovmaga2002@gmail.com>
2021-03-16 09:33:17 +0100

    Setting up the front-end with chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 // Importing dependencies...
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Button,
+  ChakraProvider,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@chakra-ui/react";
 import "./App.css";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { AddIcon, Search2Icon } from "@chakra-ui/icons";
 
 // Creating a Apollo client side with the server
 const client = new ApolloClient({
@@ -10,13 +20,42 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+// App function component
+// Apollo Provider connecting with client...
 function App() {
   return (
-    <ChakraProvider>
-      <div className="App">
-        <h1>Chakra ui</h1>
-      </div>
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <div className="App">
+          <Box>
+            <Flex alignItems="center">
+              <h1 className="App_Title">Contact List</h1>
+              <Badge ml="3" fontSize="15px">
+                Salire AS
+              </Badge>
+            </Flex>
+          </Box>
+
+          <Box width="full" px="50px" my="20px">
+            <Flex justifyContent="center">
+              <InputGroup size="lg">
+                <InputLeftAddon children={<Search2Icon />} />
+                <Input placeholder="Search for a contact..." />
+              </InputGroup>
+              <Button
+                size="lg"
+                leftIcon={<AddIcon />}
+                colorScheme="pink"
+                variant="solid"
+                ml="25px"
+              >
+                Add A Contact
+              </Button>
+            </Flex>
+          </Box>
+        </div>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
